8fc03c538a0f2d4feb3cb94f00b29e0ed1faff8f
Sergey Tyumentsev <samposebe@ngs.ru>
2022-09-06 19:41:09 +0800

    :задача 8, начало

---
+++ Семинар1_ДЗ_004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число:");
+string var = Console.ReadLine();
+int number = int.Parse(var);
+
+if (number > 1)
+{
+     int n = number;
+     for (int i = 2; i <= n; i++);
+          Console.Write($"{n}");
+}
+else
+{
+Console.Write("Число меньше или равно 1.");
+}
